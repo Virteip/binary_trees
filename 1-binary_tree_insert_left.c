4029a33407591753368d16489de6c9c64e106550
Virteip <sergiopietri@gmail.com>
2020-03-11 15:35:36 -0500

    [Fixing 1]

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (!parent)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 	binary_tree_t *left = binary_tree_node(parent, value);
