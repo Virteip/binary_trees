8c3f4badc3dfac5a0fffd2e5cdba8a14560c2ef3
Virteip <sergiopietri@gmail.com>
2020-03-11 15:50:24 -0500

    [Update]

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *right = binary_tree_node(parent, value);
+	binary_tree_t *right = NULL;
+
+	if(!parent)
+	{
+		return (NULL);
+	}
+
+	right = binary_tree_node(parent, value);
 
 	if (!parent || !right)
 	{
