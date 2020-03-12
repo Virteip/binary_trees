f092519bb58f93e4d2acd51532e0dc77fd90323e
Virteip <sergiopietri@gmail.com>
2020-03-12 17:14:10 -0500

    [Update 13]

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if ((tree->left == NULL) && (tree->right == NULL))
+	if (!(tree->left) && (tree->right == NULL))
 	{
 		return (0);
 	}
